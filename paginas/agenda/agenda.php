5176d11496cff49452e4ca3b2bbda5d3e3f8643d
albert.domingo.7e7 <albert.domingo.7e7@itb.cat>
2026-02-24 14:37:37 +0100

    titulos

--- paginas/agenda/agenda.php
+++ paginas/agenda/agenda.php
@@ -44,7 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nueva Tarea - Agenda</title>
+    <title>Neue Erinnerung</title>
     <link rel="stylesheet" href="../../css/agenda.css">
 </head>
 
