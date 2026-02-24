f059750cf51863c1e84a9298e740efffe0ec00ad
albert.domingo.7e7 <albert.domingo.7e7@itb.cat>
2026-02-24 14:39:51 +0100

    .

--- paginas/agenda/modificar.php
+++ paginas/agenda/modificar.php
@@ -41,7 +41,7 @@ $aufgaben = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Tareas - Modificar</title>
+    <title>Aufgabenliste</title>
     <link rel="stylesheet" href="../../css/agenda.css">
 
     <style>
