d5ccd6931097c5c9ccf9a5027991fb8ac426cbdf
albert.domingo.7e7 <albert.domingo.7e7@itb.cat>
2026-03-18 13:42:53 +0100

    a

--- code/scripts_sistema/apagador.py
+++ code/scripts_sistema/apagador.py
@@ -1,9 +1,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import urllib.parse
 import os
+import sys
+
+# Escudo para evitar que pythonw explote al no tener consola
+sys.stdout = open(os.devnull, 'w')
+sys.stderr = open(os.devnull, 'w')
 
 # === CONFIGURACIÓN ===
-PUERTO = 8000  # <-- Alineado con el 'LAN Host Port' de tu router ZTE
+PUERTO = 8000
 TOKEN_SECRETO = "Motxito@2024" 
 # =====================
 
@@ -11,32 +16,21 @@ class Manejador(BaseHTTPRequestHandler):
     def do_GET(self):
         url_analizada = urllib.parse.urlparse(self.path)
         parametros = urllib.parse.parse_qs(url_analizada.query)
-        
-        # Extraemos el token que llega desde Google Cloud
         token_recibido = parametros.get('token', [''])[0]
-        
-        # Chivato para la consola:
-        print(f"-> Petición recibida en: {url_analizada.path}")
-        print(f"-> Token esperado por el PC: '{TOKEN_SECRETO}'")
-        print(f"-> Token recibido de la Web: '{token_recibido}'")
 
         if url_analizada.path == '/apagar' and token_recibido == TOKEN_SECRETO:
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
+            self.wfile.write(b"Orden recibida. Apagando WERKSTATT en 5 segundos...")
             
-            self.wfile.write(b"Orden recibida. Ejecutando prueba...")
-            print("¡Contraseña correcta! Escribiendo archivo de prueba...")
-            
-
+            # --- MODO APAGADO REAL ---
             os.system("shutdown /s /t 5")
 
         else:
             self.send_response(403)
             self.end_headers()
             self.wfile.write(b"Acceso denegado.")
-            print("¡Contraseña INCORRECTA o ruta equivocada! Acceso denegado.\n")
 
 servidor = HTTPServer(('0.0.0.0', PUERTO), Manejador)
-print(f"WERKSTATT escuchando órdenes de apagado en el puerto {PUERTO}...")
 servidor.serve_forever()
